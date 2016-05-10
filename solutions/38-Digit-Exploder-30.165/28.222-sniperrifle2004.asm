42578ba31f61841bfe74b0e81a76eaa58dfdbf32
Sniperrifle2004 <sniperrifle2004+vba-rr@gmail.com>
2016-05-10 16:01:02 +0200

    Some attempts at 38 digit explorer

---
+++ solutions/38-Digit-Exploder-30.165/28.222-sniperrifle2004.asm
@@ -0,0 +1,63 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 38-Digit-Exploder - SIZE 28/30 - SPEED 222/167 --
+
+    COPYFROM 10
+    COPYTO   7
+    JUMP     g
+a:
+    COPYFROM 0
+    OUTBOX  
+    COPYFROM 7
+    SUB      10
+    JUMPZ    e
+    BUMPDN   7
+b:
+c:
+    COPYFROM 9
+    COPYTO   0
+d:
+    COPYFROM 3
+    SUB      [7]
+    JUMPN    a
+    COPYTO   3
+    BUMPUP   0
+    JUMP     d
+e:
+f:
+    COPYFROM 3
+    OUTBOX  
+g:
+    INBOX   
+    COPYTO   3
+    SUB      10
+    JUMPN    f
+    COPYFROM 3
+    SUB      11
+    JUMPN    c
+    BUMPUP   7
+    JUMP     b
+
+
+DEFINE LABEL 0
+eJzjZ2BguNd+IPBgc4XP0To1b7vSYr+HuYExAtmaOVlZ3BULs37Ux+Zc6Pcu/riEv0J/XXyj9LYtrbnb
+kzovb2AgAlT6Re5X8WI6QIzaUTAKRgH9AQD9dB4E;
+
+DEFINE LABEL 3
+eJwTZmBgiG88EBjfuL7qdr3+Ovk63YtAIYbXrYtjkzo/JMh196eu6t9Z9n3CwobvE251T+2btVSwt23t
+weZfC9wq9kzf2VC0VXGyx4vamf//M4yCUTAKhhwAAMChJp4;
+
+DEFINE LABEL 7
+eJzjY2BgKIhYZfE8erbdotgrnleTcopb0hJmhWROXc6R8XFJT2b36oVZRVuNsm6sW5g1YeXkysWxftWm
+rgyjYBSMgmEBAFzHGfo;
+
+DEFINE LABEL 9
+eJzjZmBgmOzV7sYY0eQxN0nGf3OyeOCa4PudG/zmL5ZxP359q6fy1XsRLRcOZ7ZcYBgFo2AUDDsAAAON
+E2k;
+
+DEFINE LABEL 10
+eJzjZGBgkCmbFx9cpr+utPTVSSCXwT07JcSv2ifuYPPMRNXZtikMo2AUjIJhCwCjqgxY;
+
+DEFINE LABEL 11
+eJzjY2BgSEzXy1qY5dW4LVt4+rbsmo2xOez7gMIMp3MMu2XKejt2Nng1vprEXQESW9STEtLbrb+OYRSM
+glEwLAAAl0IUZA;
+
---
+++ solutions/38-Digit-Exploder-30.165/69.162-sniperrifle2004.asm
@@ -0,0 +1,123 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 38-Digit-Exploder - SIZE 69/30 - SPEED 162/167 --
+
+    COPYFROM 11
+    ADD      11
+    ADD      11
+    COPYTO   8
+    COPYFROM 10
+    ADD      10
+    ADD      10
+    COPYTO   7
+    JUMP     h
+a:
+    COPYFROM 9
+    COPYTO   1
+b:
+    COPYFROM 3
+    SUB      7
+    JUMPN    c
+    COPYTO   3
+    BUMPUP   1
+    BUMPUP   1
+    BUMPUP   1
+    JUMP     b
+c:
+d:
+    COPYFROM 3
+    SUB      10
+    JUMPN    f
+    COPYTO   3
+    BUMPUP   1
+    JUMP     d
+e:
+    COPYFROM 0
+    OUTBOX  
+f:
+    COPYFROM 1
+    OUTBOX  
+g:
+    COPYFROM 3
+    OUTBOX  
+h:
+    INBOX   
+    COPYTO   3
+    SUB      10
+    JUMPN    g
+    COPYFROM 3
+    SUB      11
+    JUMPN    a
+    COPYFROM 9
+    COPYTO   0
+    COPYTO   1
+i:
+    COPYFROM 3
+    SUB      8
+    JUMPN    j
+    COPYTO   3
+    BUMPUP   0
+    BUMPUP   0
+    BUMPUP   0
+    JUMP     i
+j:
+k:
+    COPYFROM 3
+    SUB      11
+    JUMPN    l
+    COPYTO   3
+    BUMPUP   0
+    JUMP     k
+l:
+m:
+    COPYFROM 3
+    SUB      7
+    JUMPN    n
+    COPYTO   3
+    BUMPUP   1
+    BUMPUP   1
+    BUMPUP   1
+    JUMP     m
+n:
+o:
+    COPYFROM 3
+    SUB      10
+    JUMPN    e
+    COPYTO   3
+    BUMPUP   1
+    JUMP     o
+
+
+DEFINE LABEL 0
+eJyTYmBgkNPtSg/TrZxgovd/HpDLcFRXddMiPcFdi/T6zoTpZuwM07Vbka23faan/sGpcQayk33MZSdn
+WUtMrbcRnn7G9uMSZjv2fSB9hh4lkww9vi1y87RcJe5zcssx/617Kv0i96t4MR1gGAWjYBQMSgAAOVwm
+dw;
+
+DEFINE LABEL 1
+eJwTZmBgUA6vzrWIYKu6F9HcszwieTdQiEHHNyVENjMwxrs4MAbEP1qnknep5k/t7JrKCS9q+WZatK8+
+sKXV/3BJk//hj8Xs+wLy2PcxjIJRMAqGHAAAIVIdqA;
+
+DEFINE LABEL 3
+eJwTZmBgiG88EBjfuL7qdr3+Ovk63YtAIYbXrYtjkzo/JMh196eu6t9Z9n3CwobvE251T+2btVSwt23t
+weZfC9wq9kzf2VC0VXGyx4vamf//M4yCUTAKhhwAAMChJp4;
+
+DEFINE LABEL 7
+eJyTZWBgWGln6hrhZOp63rXJo85Txv+v/4cEef87GQmu6aW/3NdXpfh2dMUHscw9FzR/cXxQ7vYNfqsP
+WHr7H851/nvQ2TFyP9AIhp9lQsELym1Tgsumt8iUrZ17vdzmNEj8dM5R/6/Vp3y7lnrasm70s36zvNaL
+YRSMglEwaAAAIBww8g;
+
+DEFINE LABEL 8
+eJyTY2BgSPGNMN8Tae+on1ThszuNOXJm+te43WnPUt7H7Su5Fppeei9iYUN2rGH32fhjkzsSfRYFJjov
++xNjuUref8LK2b7dq4FGMMjXaXuG120Mv1QzqQ3E/1v3Ouli54cEo5lhUSD+zGnHddinx5iyT08ql53x
+fAXDKBgFo2BQAADYgTIp;
+
+DEFINE LABEL 9
+eJzjZmBgmOzV7sYY0eQxN0nGf3OyeOCa4PudG/zmL5ZxP359q6fy1XsRLRcOZ7ZcYBgFo2AUDDsAAAON
+E2k;
+
+DEFINE LABEL 10
+eJzjZGBgkCmbFx9cpr+utPTVSSCXwT07JcSv2ifuYPPMRNXZtikMo2AUjIJhCwCjqgxY;
+
+DEFINE LABEL 11
+eJzjY2BgSEzXy1qY5dW4LVt4+rbsmo2xOez7gMIMp3MMu2XKejt2Nng1vprEXQESW9STEtLbrb+OYRSM
+glEwLAAAl0IUZA;
+
